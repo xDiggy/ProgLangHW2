71bf214d76328279e8985f71513a3125b2b496a0
Antonio Martinez III <toniom4172@gmail.com>
2023-10-26 15:53:00 -0400

    Part a+b done for concurrent

--- concurrent/graph_stats.erl
+++ concurrent/graph_stats.erl
@@ -87,12 +87,18 @@ doPartByAll(Data, GodMap) ->
             % fwrite("LMAO: ~p\n", [LMAO]),
             % fwrite("LOL: ~p\n", [LOL]),
 
+            X = #partition{id=PartitionID, nodes=NodeList},
+
             ExistingColors = maps:get("AllColors", NewNewMap),
             NewColors = addAllColors(Colors, ExistingColors),
             NewNewNewMap = maps:put("AllColors", NewColors, NewNewMap),
+            OldPartitions = maps:get("AllPartitions", NewNewNewMap),
+            NewNewNewNewMap = maps:put("AllPartitions", [X|OldPartitions], NewNewNewMap),
+            % fwrite("---------------------\n"),
+            % fwrite("FatBoi ~p\n", [NewNewNewNewMap]),
             % fwrite("NewColors: ~p\n", [NewColors]),
 
-            doPartByAll(T, NewNewNewMap)
+            doPartByAll(T, NewNewNewNewMap)
 
     end.
 
@@ -251,10 +257,146 @@ doPartByAllDriver(FilePath, A_out_file, B_out_file) ->
     % fwrite("--------- TESTING ---------\n"),
 
     {ok, A} = file:open(A_out_file, [write]),
-    countEdgesOfColor(AllColors, AllNodes, A).
+    countEdgesOfColor(AllColors, AllNodes, A),
 
+    BetterMap = makePartitionsUseful(DumbassMap),
+    fwrite("---------- DO PART B ----------\n"),
+    {ok, B} = file:open(B_out_file, [write]),
+    startPartB(BetterMap, B).
 
+startPartB(Map, B_out_file) -> 
+    AllNodes = maps:get("AllNodes", Map),
+    AllPartitions = maps:get("AllPartitions", Map),
+    % fwrite("AllPartitions: ~p\n", [AllPartitions]),
+    End = findShitFromPartition(lists:reverse(AllPartitions), AllNodes, B_out_file),
+    End2 = lists:sort(findUID(End)),
+    % fwrite("End: "),
+    io:format(B_out_file, "G: ", []),
+    printNodes(End2, B_out_file).
+    % fwrite("\n").
 
+findShitFromPartition(Parts, AllNodes, B_out_file) -> 
+    case Parts of
+        [] -> [];
+        [H|T] ->
+            X = findAllEdgesInPartition(H#partition.nodes),
+            AllEdgesInPart = removeMirroredEdges([X]),
+            % fwrite("AllEdgesInPart: ~p\n", [AllEdgesInPart]),
+            AllNodesFromAllEdges = findAllNodesFromEdges(AllEdgesInPart, AllNodes),
+            UNodes = uniqueNodes(AllNodesFromAllEdges),
+            % fwrite("AllNodesFromAllEdges: ~p\n", [UNodes]),
+            AllNodeDegrees = degreeOfNodes(UNodes, []),
+            % fwrite("AllNodeDegrees: ~p\n", [AllNodeDegrees]),
+            MaxDegree = findMaxdegree(AllNodeDegrees),
+            % fwrite("MaxDegree: ~p\n", [MaxDegree]),
+            EdgesWithMaxDegree = edgesWithMaxDegree(MaxDegree, AllNodeDegrees),
+            % fwrite("partition ~p: ", [H#partition.id]),
+            io:format(B_out_file, "partition ~p: ", [H#partition.id]),
+            printNodes(lists:sort(EdgesWithMaxDegree), B_out_file),
+            % fwrite("\n"),
+            io:format(B_out_file, "\n", []),
+            EdgesWithMaxDegree ++ findShitFromPartition(T, AllNodes, B_out_file)
+    end.
+
+findUID(IDS) ->
+    case IDS of
+        [] -> [];
+        [H|T] ->
+            case lists:member(H, T) of
+                true -> findUID(T);
+                false -> [H] ++ findUID(T)
+            end
+    end.
+
+printNodes(Nodes, B_out_file) ->
+    case Nodes of
+        [] -> ok;
+        [H|T] ->
+            % fwrite("~p", [H]),
+            io:format(B_out_file, "~p", [H]),
+            case T of
+                [] -> ok;
+                _ -> io:format(B_out_file, ",", [])
+                % _ -> fwrite(",")
+            end,
+            printNodes(T, B_out_file)
+    end.
+
+edgesWithMaxDegree(Degree, Nodes) ->
+    case Nodes of
+        [] -> [];
+        [H|T] ->
+            case nth(2, H) == Degree of
+                true -> [nth(1, H)] ++ edgesWithMaxDegree(Degree, T);
+                false -> edgesWithMaxDegree(Degree, T)
+            end
+    end.
+
+findMaxdegree(Tuples) ->
+    case Tuples of
+        [] -> 0;
+        [H|T] ->
+            X = findMaxdegree(T),
+            case nth(2, H) > X of
+                true -> nth(2, H);
+                false -> X
+            end
+    end.
+
+uniqueNodes(Nodes) ->
+    case Nodes of
+        [] -> [];
+        [H|T] ->
+            case lists:member(H#node.id, nodeList(T)) of
+                true -> uniqueNodes(T);
+                false -> [H] ++ uniqueNodes(T)
+            end
+    end.
+
+findAllNodesFromEdges(Edges, AllNodes) ->
+    case Edges of
+        [] -> [];
+        [H|T] ->
+            From = H#edge.from,
+            To = H#edge.to,
+            FromNode = findNodeFromID(From, AllNodes),
+            ToNode = findNodeFromID(To, AllNodes),
+            [FromNode, ToNode] ++ findAllNodesFromEdges(T, AllNodes)
+    end.
+
+findNodeFromID(ID, Nodes) ->
+    case Nodes of
+        [] -> [];
+        [H|T] ->
+            case H#node.id == ID of
+                true -> H;
+                false -> findNodeFromID(ID, T)
+            end
+    end.
+
+findAllEdgesInPartition(Nodes) ->
+    case Nodes of
+        [] -> [];
+        [H|T] ->
+            AllEdges = H#node.edges,
+            AllEdges ++ findAllEdgesInPartition(T)
+    end.
+
+nodeList(Nodes) ->
+    case Nodes of
+        [] -> [];
+        [H|T] ->
+            [H#node.id] ++ nodeList(T)
+    end.
+
+degreeOfNodes(Nodes, Return) ->
+    case Nodes of
+        [] -> Return;
+        [H|T] ->
+            NewReturn = [[H#node.id, lengthOf(H#node.edges)]] ++ Return,
+            NR = degreeOfNodes(T, NewReturn),
+            NR
+    end.
 
 createListOfEdges(Input, Edges) ->
     case Input of
@@ -298,6 +440,44 @@ parse_partitions(Input_file_path, A_out, B_out) ->
     {ok, File} = file:open(Input_file_path, [read]),
     % doPartByLine(File).
     doPartByAllDriver(File, A_out, B_out).
+
+makePartitionsUseful(GodMap) ->
+    Partitions = maps:get("AllPartitions", GodMap),
+    Nodes = maps:get("AllNodes", GodMap),
+    SomeBS = temp(Partitions, Nodes, []),
+    BetterMap = maps:put("AllPartitions", SomeBS, GodMap),
+    BetterMap.
+
+temp(Parts, Nodes, NewParts) ->
+    case Parts of
+        [] -> NewParts;
+        [H|T] ->
+            NewNodes = partitionToListOfNodes(H#partition.nodes, Nodes, []),
+            NewPartition = #partition{id=H#partition.id, nodes=NewNodes},
+            temp(T, Nodes, NewParts ++ [NewPartition])
+    end.
+
+partitionToListOfNodes(OldNodes, RealNodes, Return) ->
+    case OldNodes of
+        [] -> Return;
+        [H|T] ->
+            NodeID = H#node.id,
+            Node = idToNode(NodeID, RealNodes),
+            NewReturn = [Node] ++ Return,
+            Again = partitionToListOfNodes(T, RealNodes, NewReturn),
+            Again
+    end.
+
+idToNode(Id, AllNodes) ->
+    case AllNodes of
+        [] -> [];
+        [H|T] ->
+            case H#node.id == Id of
+                true -> H;
+                false -> idToNode(Id, T)
+            end
+    end.
+
 
 start(Input_file_path, Part_a_output_file_path, Part_b_output_file_path) ->
     % code starts here
